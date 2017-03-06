423daa6b486d177cae385919d80de7b295385e77
miriampc <miiriampc@gmail.com>
2017-03-06 00:13:58 -0500

    agregando jugadas ganadas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
-var btn1=var btn1=document.getElementById('1');
-var btn2=var btn1=document.getElementById('2');
-var btn3=var btn1=document.getElementById('3');
-var btn4=var btn1=document.getElementById('4');
-var btn5=var btn1=document.getElementById('5');
-var btn6=var btn1=document.getElementById('6');
-var btn7=var btn1=document.getElementById('7');
-var btn8=var btn1=document.getElementById('8');
-var btn9=var btn1=document.getElementById('9');
+var btn1=document.getElementById('1');
+var btn2=document.getElementById('2');
+var btn3=document.getElementById('3');
+var btn4=document.getElementById('4');
+var btn5=document.getElementById('5');
+var btn6=document.getElementById('6');
+var btn7=document.getElementById('7');
+var btn8=document.getElementById('8');
+var btn9=document.getElementById('9');
 
 
 function Tictactoe(x,o){
@@ -18,12 +18,21 @@ function Tictactoe(x,o){
       || (btn7.value==this.x && btn8.value==this.x && btn9.value==this.x)
       || (btn1.value==this.x && btn4.value==this.x && btn7.value==this.x)
       || (btn2.value==this.x && btn5.value==this.x && btn8.value==this.x)
-      || (btn3.value==this.x && btn6.value==this.x && btn9.value==this.x)){
+      || (btn3.value==this.x && btn6.value==this.x && btn9.value==this.x)
+      || (btn1.value==this.x && btn5.value==this.x && btn9.value==this.x)
+      || (btn3.value==this.x && btn5.value==this.x && btn7.value==this.x)){
       alert("jugador X gana");
+    }else if((btn1.value==this.o && btn2.value==this.o && btn3.value==this.o)
+              || (btn4.value==this.o && btn5.value==this.o && btn6.value==this.o)
+              || (btn7.value==this.o && btn8.value==this.o && btn9.value==this.o)
+              || (btn1.value==this.o && btn4.value==this.o && btn7.value==this.o)
+              || (btn2.value==this.o && btn5.value==this.o && btn8.value==this.o)
+              || (btn3.value==this.o && btn6.value==this.o && btn9.value==this.o)
+              || (btn1.value==this.o && btn5.value==this.o && btn9.value==this.o)
+              || (btn3.value==this.o && btn5.value==this.o && btn7.value==this.o)){
+              alert("jugador O gana");
     }
   }
-
-
 }
 var jugador=new Tictactoe("X","O");
 jugador.winner();
